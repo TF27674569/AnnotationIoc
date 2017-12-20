94a2cfff189c4e3132b6fb78f0f9fc2039102d6e
田锋 <tianfeng@formssi.com>
2017-12-20 10:08:01 +0800

    更改字符编码集 注解删除不能映射的文本

--- view-annotation/src/main/java/org/annotation/EchoEnable.java
+++ view-annotation/src/main/java/org/annotation/EchoEnable.java
@@ -5,14 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * description£º
- * <p>
- * Created by TIAN FENG on 2017/12/19.
- * QQ£º27674569
- * Email: 27674569@qq.com
- * Version£º1.0
- */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.CLASS)
 public @interface EchoEnable {
